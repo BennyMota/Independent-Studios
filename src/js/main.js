2b8b748309cabe2bae60e7139c4e86599fcf49f8
BennyMota <bennyniice@icloud.com>
2023-04-08 21:43:19 +0200

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-window.addEventListener('scroll', function(){
+document.getElementById('nav', function(){
     var navbar = document.querySelector('navbar');
     navbar.classList.toggle('sticky', window.scrollY > 0)
 })
